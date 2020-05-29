a58b1878b9bcbb6e5e8f9e953b219cd2f99530ef
Govardhan <govardhan.yannam@gmail.com>
2020-05-29 12:41:29 +0530

    NamedQuery demonstration

--- src/main/java/com/pluralsight/repository/GoalRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/GoalRepositoryImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import java.util.List;
 import java.util.Queue;
 
@@ -24,17 +25,17 @@ public class GoalRepositoryImpl implements GoalRepository{
     }
 
     @Override
-    @SuppressWarnings({"rawTypes","unchecked"})
     public List<Goal> loadAll() {
-        Query query = em.createQuery("select g from Goal g");
+//        Query query = em.createQuery("select g from Goal g");
+        TypedQuery<Goal> query = em.createNamedQuery(Goal.FINAL_ALL_GOALS, Goal.class);
         return query.getResultList();
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public List<GoalReport> findAllGoalReports() {
-        Query query = em.createQuery("select new com.pluralsight.model.GoalReport(g.minutes,e.minutes,e.activity) " +
-                "from Goal g, Exercise e where g.id=e.id");
+//        Query query = em.createQuery("select new com.pluralsight.model.GoalReport(g.minutes,e.minutes,e.activity) " +
+//                "from Goal g, Exercise e where g.id=e.id");
+        TypedQuery<GoalReport> query = em.createNamedQuery(Goal.FINAL_GOAL_REPORTS,GoalReport.class);
         return query.getResultList();
     }
 }
